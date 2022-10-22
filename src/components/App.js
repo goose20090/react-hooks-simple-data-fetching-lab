48c68417dfcc60bca856b960f4d0b5fea515703e
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2022-10-22 22:12:39 +0100

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
 // create your App component here
+import React, {useEffect, useState} from "react";
+
+
+function App(){
+    const [dogImage, setDogImage] = useState("")
+    useEffect(()=>{
+        fetch ('https://dog.ceo/api/breeds/image/random')
+        .then ((response)=> response.json())
+        .then((returnedDogImage)=>
+        setDogImage(<img alt = "A Random Dog" src = {returnedDogImage.message}></img>),
+        // setIsLoaded(true)
+        )
+    }, [])
+
+    if (!dogImage) return <p>Loading...</p>;
+
+    return <div>{dogImage}</div>
+}
+
+export default App;
